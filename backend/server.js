bf32ff4c88e3a45e1417d0762bd5ed7d63177fcc
Olaoluwa Dayo-Olaide (pla326) <pla326@mail.usask.ca>
2025-03-22 16:12:42 -0600

    set up my necessary front and backend folders

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const nano = require('nano');
+const date=require('date-and-time');
+
+const app=express();  //instantiate the express app
+
