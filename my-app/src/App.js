7f54f5d132b0fcbd928d3616d2da360605046bee
Olaoluwa Dayo-Olaide (pla326) <pla326@mail.usask.ca>
2025-03-24 15:40:59 -0600

    tested the couchdb and server setup and they work fine, working on React frontend, created basic landing page, need to add components to allow creating a channel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+
+import message from './message.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={message} className="App-logo" alt="randostring" />
+        <p className="App-intro"> Ask your question, get the answer you need.</p>
+        <p className="App-body"> Welcome to post questions system where you are able to post questions that other users may respond
+          to provided they have an answer, and you may engage with other's questions. Got a something on your
+          mind? Post your first question today, chances are someones willing to help explain!
+        </p>   
       </header>
     </div>
   );
